af4d31a6fbf08cacd42c97386cca60d5c24ebce6
sothengski <sothengchheang@Sothengs-MacBook-Pro.local>
2025-11-09 13:58:06 -0800

    feat(theme): add basic app theme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Modular Todo App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      // title: 'Modular Todo App',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
+      restorationScopeId: 'app',
+      onGenerateTitle: (BuildContext context) => 'Flutter HB Salesforce',
+      theme: ThemeData(
+        // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
+        useMaterial3: true,
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black87,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black, // background (button) color
+            foregroundColor: Colors.white, // foreground (text) color
+          ),
+        ),
+      ),
     );
   }
 }
